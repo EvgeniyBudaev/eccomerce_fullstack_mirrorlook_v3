0116c4d738058a6acbac1b5f0cbfef872382cdd4
Evgeniy Budaev <budaev.e@gmail.com>
2021-06-29 08:25:24 +0300

    add global styles

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,6 +6,10 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <>
     <Head>
       <title>MirrorLook интернет-магазин зеркал </title>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+                rel="stylesheet"/>
     </Head>
     <Component {...pageProps} />
   </>;
