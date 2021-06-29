841dbec279999a90c405c3d5bc4c5d0a6383bf74
Evgeniy Budaev <budaev.e@gmail.com>
2021-06-29 06:32:28 +0300

    Компонент Document

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,14 @@
 import {AppProps} from "next/dist/next-server/lib/router/router";
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>MirrorLook интернет-магазин зеркал </title>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
