7406b0aef355e1c099ab8b5dcc327283a96c39ac
Evgeniy Budaev <budaev.e@gmail.com>
2021-06-30 07:52:56 +0300

    remove link to fonts

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -10,10 +10,6 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <meta name="description" content="Интернет магазин зеркал"/>
       <meta name="keywords" content="зеркало, магазин, купить, цена"/>
       <title>MirrorLook интернет-магазин зеркал </title>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
-                rel="stylesheet"/>
     </Head>
     <Component {...pageProps} />
   </>;
