0cc01585a31d3bec67a99b01c332879845796c9a
Evgeniy Budaev <budaev.e@gmail.com>
2021-07-03 17:29:43 +0300

    remove bootstrap

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import Head from "next/head";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
