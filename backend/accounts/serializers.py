23703ba6524385158a299e48ae9dc062379b778a
Evgeniy Budaev <budaev.e@gmail.com>
2021-09-12 12:45:55 +0300

    fix redoc

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -8,5 +8,6 @@ User = get_user_model()
 class UserSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
+        ref_name = "_user"
         fields = ('id', 'first_name', 'last_name', 'phone_number', 'is_active',
                   'is_staff', 'is_admin', 'is_superuser', 'email', 'password')
