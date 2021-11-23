ac2d77b4d269f57d3e12154e6a01734132b07338
Evgeniy Budaev <budaev.e@gmail.com>
2021-11-23 11:06:45 +0300

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [products, setProducts] = useState([]);
 
   const getProducts = async () => {
-    const baseUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_DOMAIN : process.env.REACT_APP_DEV_DOMAIN;
+    // const baseUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_DOMAIN : process.env.REACT_APP_DEV_DOMAIN;
+    const baseUrl = "http://backend:8000/";
     console.log("baseUrl: ", baseUrl);
     const response = await axios.get(`${baseUrl}api/v1/products/?catalog_slug=mirrors`);
     console.log("response", response);
