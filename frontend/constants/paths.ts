3ca79835efb4fcbc7f4ff646e051b2f749df6c28
Evgeniy Budaev <budaev.e@gmail.com>
2022-02-13 18:54:10 +0300

    fix for deploy

--- frontend/constants/paths.ts
+++ frontend/constants/paths.ts
@@ -3,6 +3,5 @@ const browser = typeof window !== "undefined";
 
 export const backendBase =
   !dev && !browser ? "http://backend:8000/" : process.env.NEXT_PUBLIC_DOMAIN;
-// dev: "http://127.0.0.1:8000/"
-// prod: "http://backend:8000/"
-// export const backendBase = "http://backend:8000/";
+
+// export const backendBase = "http://127.0.0.1:8000/";
