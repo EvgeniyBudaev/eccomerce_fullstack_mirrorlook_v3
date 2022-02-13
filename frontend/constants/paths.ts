e260f07cd7e72e4091da05cf2c8803e82e045478
Evgeniy Budaev <budaev.e@gmail.com>
2022-02-13 15:59:17 +0300

    constants/path

--- frontend/constants/paths.ts
+++ frontend/constants/paths.ts
@@ -1,8 +1,8 @@
 const dev = process.env.NODE_ENV !== "production";
 const browser = typeof window !== "undefined";
 
-// export const backendBase =
-//   !dev && !browser ? "http://backend:8000/" : process.env.NEXT_PUBLIC_DOMAIN;
+export const backendBase =
+  !dev && !browser ? "http://backend:8000/" : process.env.NEXT_PUBLIC_DOMAIN;
 // dev: "http://127.0.0.1:8000/"
 // prod: "http://backend:8000/"
-export const backendBase = "http://backend:8000/";
+// export const backendBase = "http://backend:8000/";
